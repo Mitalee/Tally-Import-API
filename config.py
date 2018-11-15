c282041059f2feedb31372109be66c14606f9a7f
Manas Mangaonkar <manas.man95@gmail.com>
2018-11-15 23:36:24 +0530

    V0.1

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+url = 'http://192.168.1.105:9002'
+
+DEBUG = True
+
+
---
+++ tapi.py
@@ -0,0 +1,43 @@
+import os
+import config
+import requests
+import tasks 
+ 
+
+from xml.etree import ElementTree as ET
+from flask import Flask, jsonify, abort, make_response
+from flask_restful import Api, Resource, reqparse, fields, marshal
+from flask_httpauth import HTTPBasicAuth
+ 
+
+tapi = Flask(__name__)
+api = Api(tapi)
+auth = HTTPBasicAuth()
+
+# view to ping tally server and check if running or not
+@tapi.route('/tally/api/v1.0/pingserver')
+def ping_tally():
+	p = tasks.ping_tally()
+	return(p)
+
+# view to import vouchers from csv using dataframe and send to taly
+@tapi.route('/tally/api/v1.0/voucherimport/<path:filename>')
+def batch_voucher_import(filename):
+	try:
+		request_xml = tasks.create_voucher_request(filename)
+		result = tasks.send_tally_request(tally_req=request_xml)
+		return(result)
+	except Exception as e:
+		return(str(e))
+# view to import stock items from dataframe and send to tally
+@tapi.route('/tally/api/v1.0/stockitemsimport/<path:filename>')
+def batch_import_stockitems(filename):
+	#try:
+	request_xml = tasks.create_stockitem_request(filename,chunksize=10)
+	result = tasks.send_tally_request(tally_req=request_xml)
+	return(result)
+	#except Exception as e:
+	return(str(e))
+
+if __name__ == '__main__':
+    tapi.run(debug=True)
