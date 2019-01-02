aaea42a9079d80c5b556e3b1ede7a002fe2859f7
Manas Mangaonkar <manas.man95@gmail.com>
2019-01-02 17:19:26 +0530

    pep fix

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
 
-url = 'http://192.168.1.102:9002' # Insert your tally server url here
+url = 'http://192.168.1.102:9002'  # Insert your tally server url here
 host = '0.0.0.0'
-port = 80 
+port = 80
 
-#DEBUG = True 
-
-
+# DEBUG = True
